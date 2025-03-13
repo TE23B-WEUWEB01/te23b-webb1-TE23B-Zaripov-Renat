6b475ae2f5e645bc965c756da2b862f05ee7a239
Renat Zaripov <renat.zaripov@elev.ga.ntig.se>
2025-03-13 11:06:46 +0100

    Tränat js, variabler och konkatenering

---
+++ kapitel 8/Intro/skript.js
@@ -0,0 +1,12 @@
+// var ålder = 56;
+// var tal1 = 12;
+// var tal2 = 24;
+// var summa = tal1 + tal2;
+// alert(tal1 + "+" + tal2 + "=" + summa);
+var förnamn = "Renat";
+var efternamn = "Zaripov";
+var helaNamnet = förnamn + " " + efternamn;
+var hobby = "fotboll";
+var kostnad = "2000"
+var mening = helaNamnet + "'s hobby är "  + hobby + ".\nHanns hobby kostar" + " " + kostnad + "kr/år"
+alert(mening);
