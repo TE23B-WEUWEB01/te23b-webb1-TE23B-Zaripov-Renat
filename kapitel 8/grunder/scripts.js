1bf74570683353bda284f1a0e99350cc823b0711
Renat Zaripov <renat.zaripov@elev.ga.ntig.se>
2025-03-14 14:04:49 +0100

    Tränat på prompt() och att skriva i webbfönstret

---
+++ kapitel 8/grunder/scripts.js
@@ -0,0 +1,46 @@
+// // var dag = "fredag";
+// // alert(dag);
+// // console.log("Idag är det " + dag);
+
+// // Var är du född?
+// var stad = prompt("Var är du född?");
+// console.log("Du är född i " + stad);
+
+// // Vi gör en enkel captcha
+// var gissning = prompt("Vad är 23 + 45?");
+// if (gissning == 68) {
+//     alert("Du är en människa");
+// }   else {
+//     alert("Du är en robot");
+// }
+// // Spamchek med två slumpade tal
+
+// var slumptal1 = Math.round(Math.random() * 100);
+// var slumptal2 = Math.round(Math.random() * 100);
+// var facit = slumptal1 + slumptal2;
+
+// // Fråga användaren vad är ?? + ??
+// var gissning = prompt("Vad är " + slumptal1 + " + " + slumptal2 + "?");
+// if (gissning == facit) {
+//     document.writeln("<h1>Du är en männiksa! 😘</h1>");
+// }   else {
+//     document.writeln("<h1>Du är en robot! 💻</h1>");
+// }
+
+
+// Uppgift: skapa en miniräknare
+// Läs in två tal
+// Skriv ut valfri matematisk operation: + - / *
+// var tal1 = prompt("Ange ett tal1");
+// var tal2 = prompt("Ange ett tal2");
+// var summa = Number(tal1) + Number(tal2);
+// document.writeln("Summan är " + summa);
+
+// Uppgift: skatteuträckning
+// Läs in: inkomst, tex 30.000:-
+// läs in: skatt, tex 30%
+// Skriv ut: "Din inkomst efter skatt är xxx med yy% i skatt."
+var brutto = prompt("Vad är din lön före skatt?");
+var skatt = prompt("Vad är din skatt?")
+var inkomst = Number(brutto) * (100 - Number(skatt)) / 100;
+document.writeln("Din inkomst efter skatt är " + inkomst + " med " + skatt + "% i skatt. ");
